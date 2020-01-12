31b675671e4b822db987684d265342ee35ea69b0
chengwh <1090610426@qq.com>
2020-01-12 21:52:28 +0800

    notSave

--- bravobest-web/src/main/java/com/bravo/bravobest/BravobestWebApplication.java
+++ bravobest-web/src/main/java/com/bravo/bravobest/BravobestWebApplication.java
@@ -1,13 +1,16 @@
 package com.bravo.bravobest;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@MapperScan("com.bravo.bravobest.dao")
 public class BravobestWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BravobestWebApplication.class, args);
+        SpringApplication.run(com.bravo.bravobest.BravobestWebApplication.class, args);
     }
 
 }

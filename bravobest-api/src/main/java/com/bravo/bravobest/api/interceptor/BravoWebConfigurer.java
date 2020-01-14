176e315a8673bd12ab2f33386ed9aa076fb35bed
chengwh <1090610426@qq.com>
2020-01-14 18:11:55 +0800

    添加登录拦截器

---
+++ bravobest-api/src/main/java/com/bravo/bravobest/api/interceptor/BravoWebConfigurer.java
@@ -0,0 +1,29 @@
+package com.bravo.bravobest.api.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 拦截器配置
+ */
+@Configuration
+public class BravoWebConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    // 这个方法是用来配置静态资源的，比如html，js，css，等等
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/login", "/register");
+    }
+
+}

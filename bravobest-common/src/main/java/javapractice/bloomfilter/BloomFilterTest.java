1683f10062cf4bd4e4a900aef5a3ca24c3cbf7d6
chengwh <1090610426@qq.com>
2020-12-03 17:55:54 +0800

    Bloom过滤器实现

---
+++ bravobest-common/src/main/java/javapractice/bloomfilter/BloomFilterTest.java
@@ -0,0 +1,61 @@
+package javapractice.bloomfilter;
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+import java.nio.charset.Charset;
+
+/**
+ * bloomFilter
+ */
+public class BloomFilterTest {
+
+    /*布隆过滤器(Bloom Filter) 是一种节省空间的概率数据结构， 用来测试一个元素是否在一个集合里。
+    它实际上是一个很长的二进制向量和一系列随机映射函数。相比于传统的List、Set、Map 等数据结构，
+    它更高效、占用空间更少，但是缺点是其返回的结果是概率性的，而不是确切的。*/
+
+    /*原理：
+    可以拆分成两个环节：
+    插入，当一个元素被加入集合时，通过K个散列（Hash）函数将这个元素映射成一个位数组中的K个点，把它们置为1。
+    查找，当需要判断这个元素是否存在时，我们只要看看这些点是不是都是1就，大约知道集合中有没有它了，如果这些点有任何一个0，
+    则被检元素一定不在；如果都是1，则被检元素很可能存在。*/
+
+    /*优点
+    1.节省存储空间
+    2.查找速度快。
+
+    缺点
+    1.存在误判，可能要查到的元素并没有在容器中，但是hash之后得到的k个位置上值都是1。
+    2.删除困难。一个放入容器的元素映射到bit数组的k个位置上是1，删除的时候不能简单的直接置为0，可能会影响其他元素的判断。*/
+
+
+    public static void main(String[] args) {
+
+        int total = 1000000;
+
+        BloomFilter<CharSequence> bloomFilter = BloomFilter.create(Funnels.stringFunnel(Charset.forName("utf-8")), total);
+
+        //可以自己规定错误率，默认0.03%
+        //BloomFilter<CharSequence> bloomFilter1 = BloomFilter.create(Funnels.stringFunnel(Charset.forName("utf-8")), total, 0.0001);
+
+        for (int i = 0; i < total; i++){
+            bloomFilter.put("" + i);
+        }
+
+        for (int i = 0; i < total; i++){
+            if (!bloomFilter.mightContain(i + "")){
+                System.out.println(i + "应该在过滤器里，但是却不在！");
+            }
+        }
+
+        int count = 0;
+        for (int i = total; i < total * 2 ; i++){
+            if (bloomFilter.mightContain(i + "")){
+                System.out.println(i + "不应该在过滤器里，但是却在！");
+                count++;
+            }
+        }
+        System.out.println("不应该在过滤器里，但是却在的个数总共有" + count + "个！");
+        System.out.println("不应该在过滤器里，但是却在的个数占总数的比例为" + (double)count/total + "！");
+    }
+}
---
+++ bravobest-common/src/main/java/javapractice/reentrantLockTest/ReentrantLockTest.java
@@ -0,0 +1,331 @@
+package javapractice.reentrantLockTest;
+
+import java.time.LocalDate;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReentrantLockTest {
+
+    private Lock lock = new ReentrantLock();
+    private Condition condition1 = lock.newCondition();
+    private boolean has = false;
+
+    private Lock fairLock = new ReentrantLock(true);//公平锁
+    private Lock unFairLock = new ReentrantLock();//非公平锁
+
+    //读写锁
+    private ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+
+    private void lockTest(){
+        lock.lock();
+        try{
+            for (int i = 0;i <10 ; i++){
+                System.out.println("线程"+ Thread.currentThread().getName() + "---打印---" + i);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+
+    private void lockInterruptiblyTest() throws InterruptedException {
+        /*
+        lockInterruptibly()为中断方法，当通过这个方法去获取锁时，如果线程正在处于获取锁状态，
+         则该线程能够响应中断（中断线程的等待状态），抛出中断异常。也就使说，当两个线程同时通过
+         lock.lockInterruptibly()获取某个锁时，如果线程A获取到了锁，而线程B在等待状态，
+         那么对线程B调用threadB.interrupt()方法能够中断线程B的等待状态
+         */
+        lock.lockInterruptibly();
+        try{
+            System.out.println("线程"+ Thread.currentThread().getName() + "获得锁！");
+            while (true) {
+//                System.out.println("线程"+ Thread.currentThread().getName() + "---打印---");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("线程"+ Thread.currentThread().getName() + "异常！");
+        }
+        finally {
+            lock.unlock();
+            System.out.println("线程"+ Thread.currentThread().getName() + "释放锁！");
+        }
+    }
+
+    private void conditionAwaitTest(){
+        lock.lock();
+        try{
+            System.out.println("线程" + Thread.currentThread().getName() + "获得了锁");
+            System.out.println("线程" + Thread.currentThread().getName() + "等待");
+            //等待
+            condition1.await();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("线程" + Thread.currentThread().getName() + "释放了锁");
+            lock.unlock();
+        }
+    }
+
+    private void conditionSignalTest(){
+        lock.lock();
+        try{
+            System.out.println("线程" + Thread.currentThread().getName() + "获得了锁");
+            System.out.println("线程" + Thread.currentThread().getName() + "唤醒");
+            //通知
+            condition1.signal();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("线程" + Thread.currentThread().getName() + "释放了锁");
+            lock.unlock();
+        }
+    }
+
+
+    /**
+     * 生产
+     */
+    private void produce(){
+        lock.lock();
+        try{
+//            System.out.println("线程" + Thread.currentThread().getName() + "获得了锁");
+            while (has == true){
+                condition1.await();//有就等待暂时不生产
+            }
+            System.out.println("线程" + Thread.currentThread().getName() + "开始生产");
+            has = true;//开始生产
+            condition1.signal();//通知消费
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * 消费
+     */
+    private void consume() {
+        lock.lock();
+        try {
+//            System.out.println("线程" + Thread.currentThread().getName() + "获得了锁");
+            while (has == false) {
+                condition1.await();//没有了暂停消费
+            }
+            System.out.println("线程" + Thread.currentThread().getName() + "开始消费");
+            has = false;//消费完了
+            condition1.signal();//通知生产
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private void fairLockTest(){
+        fairLock.lock();
+        System.out.println(Thread.currentThread().getName() + "获得了锁！");
+        fairLock.unlock();
+    }
+
+    private void unFairLockTest(){
+        unFairLock.lock();
+        System.out.println(Thread.currentThread().getName() + "获得了锁！");
+        unFairLock.unlock();
+    }
+
+    private void readLockTest() throws InterruptedException {
+        readWriteLock.readLock().lock();
+        System.out.println(Thread.currentThread().getName() + "获得了读锁！");
+        // 睡眠，保证B线程进来时A线程还是获取锁状态
+        Thread.sleep(3000);
+        System.out.println(Thread.currentThread().getName() + "释放了读锁！");
+        readWriteLock.readLock().unlock();
+    }
+
+    private void writeLockTest() throws InterruptedException {
+        readWriteLock.writeLock().lock();
+        System.out.println(Thread.currentThread().getName() + "获得了写锁！");
+        // 睡眠，保证B线程进来时A线程还是获取锁状态
+        Thread.sleep(3000);
+        System.out.println(Thread.currentThread().getName() + "释放了写锁！");
+        readWriteLock.writeLock().unlock();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        //1.lock和unlock的使用
+        /*
+        ReentrantLockTest lockTest = new ReentrantLockTest();
+        //线程1
+        System.out.println("线程1准备");
+        new Thread(() -> lockTest.lockTest()).start();
+        //线程2
+        System.out.println("线程2准备");
+        new Thread(() -> lockTest.lockTest()).start();
+        */
+
+
+        //2.使用lockInterruptibly
+        /*
+        ReentrantLockTest lockTest1 = new ReentrantLockTest();
+        Thread t1 = new Thread(() -> {
+            try {
+                lockTest1.lockInterruptiblyTest();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println("线程1中断！");
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            try {
+                lockTest1.lockInterruptiblyTest();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println("线程2中断！");
+            }
+        });
+
+        t1.setName("T1");
+        t2.setName("T2");
+
+        t1.start();
+        t2.start();
+        Thread.sleep(2000);
+
+        //在t2线程等待锁的时候，将其中断
+        t2.interrupt();
+        */
+
+        //3.利用 Condition的await()方法和signal()方法实现等待通知机制；
+        /*
+        ReentrantLockTest lockTest2 = new ReentrantLockTest();
+        Thread t1 = new Thread(() -> lockTest2.conditionAwaitTest());
+        t1.setName("T1");
+        t1.start();
+        t1.sleep(3000);
+        //唤醒
+        lockTest2.conditionSignalTest();
+        */
+
+
+        //4.使用Condition实现生产消费模式
+        /*
+        ReentrantLockTest lockTest3 = new ReentrantLockTest();
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 200; i++) {
+                lockTest3.produce();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 200; i++) {
+                lockTest3.consume();
+            }
+        });
+        t1.setName("线程1");
+        t2.setName("线程2");
+
+        t1.start();
+        t2.start();
+        */
+
+        //Lock 锁分为公平锁和非公平锁，公平锁表示通过线程加锁的顺序获取锁，非公平锁表示通过抢占机制获取锁
+        //5.公平锁
+        /*ReentrantLockTest lockTest4 = new ReentrantLockTest();
+        for (int i = 0;i < 200;i++){
+            Thread thread = new Thread(() -> lockTest4.fairLockTest());
+            thread.setName(i + "");
+            thread.start();
+        }*/
+
+        //6.非公平锁
+        /*ReentrantLockTest lockTest5 = new ReentrantLockTest();
+        for (int i = 0;i < 200;i++){
+            Thread thread = new Thread(() -> lockTest5.unFairLockTest());
+            thread.setName(i + "");
+            thread.start();
+        }*/
+
+
+       /*ReentrantLock 与  synchronized 都是 独占锁， 安全性较高，但是相对来说效率低下；
+        ReentrantReadWriteLock读写锁提供了 readLock()和writeLock()用来获取读锁和写锁；
+        读锁之间读取数据不互斥（故读锁也成为共享锁）。读锁写锁之间互斥，写锁写锁之间互斥；*/
+        //7.读锁与读锁
+        /*
+        ReentrantLockTest lockTest6 = new ReentrantLockTest();
+        Thread t1 = new Thread(() -> {
+            try {
+                lockTest6.readLockTest();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        t1.setName("线程1");
+        t1.start();
+
+        Thread t2 = new Thread(() -> {
+            try {
+                lockTest6.readLockTest();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        t2.setName("线程2");
+        t2.start();
+        */
+
+        //8.写锁与写锁
+       /* ReentrantLockTest lockTest7 = new ReentrantLockTest();
+        Thread t1 = new Thread(() -> {
+            try {
+                lockTest7.writeLockTest();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        t1.setName("线程1");
+        t1.start();
+
+        Thread t2 = new Thread(() -> {
+            try {
+                lockTest7.writeLockTest();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        t2.setName("线程2");
+        t2.start();*/
+
+        //9.读锁与写锁
+        ReentrantLockTest lockTest8 = new ReentrantLockTest();
+        Thread t1 = new Thread(() -> {
+            try {
+                lockTest8.readLockTest();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        t1.setName("线程1");
+        t1.start();
+
+        Thread t2 = new Thread(() -> {
+            try {
+                lockTest8.writeLockTest();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        t2.setName("线程2");
+        t2.start();
+
+    }
+}
